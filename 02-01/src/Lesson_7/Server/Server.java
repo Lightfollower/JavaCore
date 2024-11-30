7a5d360d1628f42f7d6439f3dc5e6a9470168809
LightFollower <LightFollower1985@gmail.com>
2024-11-30 10:31:57 +0500

    Pull lesson 7

---
+++ 02-01/src/Lesson_7/Server/Server.java
@@ -0,0 +1,60 @@
+package Lesson_7.Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.Vector;
+
+public class Server {
+    private Vector<ClientHandler> clients;
+
+    public Server() throws SQLException {
+        clients = new Vector<>();
+        ServerSocket server = null;
+        Socket socket = null;
+
+        try {
+            AuthService.connect();
+//            String test = AuthService.getNickByLoginAndPass("login1", "pass1");
+//            System.out.println(test);
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен!");
+            while (true) {
+                socket = server.accept();
+                new ClientHandler(this, socket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
+        }
+    }
+
+    public void broadcastMsg(String msg) {
+        for (ClientHandler o: clients) {
+            o.sendMsg(msg);
+        }
+    }
+
+    public void subscribe(ClientHandler client) {
+        clients.add(client);
+    }
+
+    public void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+    }
+
+}
