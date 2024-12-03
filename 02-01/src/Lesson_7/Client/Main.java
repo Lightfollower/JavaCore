2ac47b973ca0ddd5d7e553ec92b1eb2f8e657711
LightFollower <LightFollower1985@gmail.com>
2024-12-03 21:00:45 +0500

    Lesson 7 main

--- 02-01/src/Lesson_7/Client/Main.java
+++ 02-01/src/Lesson_7/Client/Main.java
@@ -6,26 +6,40 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/Lesson_7/Client/sample.fxml"));
+
     Controller controller;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Lesson_7/Client/sample.fxml"));
         Parent root = loader.load();
         controller = loader.getController();
         primaryStage.setTitle("chat 2k19");
         Scene scene = new Scene(root, 350, 370);
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.setOnCloseRequest(event -> {
+            try {
+                System.out.println("onClose");
+                controller.out.writeUTF("/end");
+                super.stop();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
-    @Override
-    public void stop() throws Exception {
-        System.out.println("somebody calls");
-        controller.out.writeUTF("/end");
-        super.stop();
-    }
+//    @Override
+//    public void stop() throws Exception {
+//        System.out.println("somebody calls");
+//        controller.out.writeUTF("/end");
+//        super.stop();
+//    }
 
     public static void main(String[] args) {
         launch(args);
